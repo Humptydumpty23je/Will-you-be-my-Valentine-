59d2d89944d3bbf05835eea21f41c9c00524fac9
Iboyi Aghogho Prosper <Iboyiaghogho387@gmail.com>
2026-08-01 20:28:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -57,11 +57,12 @@ const messages = [
     "Are you sure?",
     "Really sure??",
     "Are you positive?",
-    "Pookie please...",
+    "Baebee please...",
     "Just think about it!",
     "If you say no, I will be really sad...",
     "I will be very sad...",
     "I will be very very very sad...",
+    "You've fucked me too much to say no, baby pleaseeeeee"😭,
     "Ok fine, I will stop asking...",
     "Just kidding, say yes please! ❤️"
 ];
